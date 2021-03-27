40ebc25d3a72905ad738f6248fc391b284c2a440
Hyuma     Asada <h.asada@gemcook.com>
2021-03-27 10:30:46 +0900

    トークンが違う場合にエラー文言表示

--- pages/authentication.vue
+++ pages/authentication.vue
@@ -2,6 +2,7 @@
   <div>
     <input type="text" placeholder="トークン" v-model="token" />
     <button @click="tokenSend">認証</button>
+    <p>{{tokenErrorMessage}}</p>
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import { TokenUtil } from '@/utilities/tokenUtil';
 @Component
 export default class Authentication extends Vue {
   public token: string = '';
+  public tokenErrorMessage: string = '';
 
   public async tokenSend() {
     TokenUtil.setToken(this.token);
@@ -21,6 +23,7 @@ export default class Authentication extends Vue {
       this.$router.push('/');
     } catch (error) {
       console.log(error.response);
+      this.tokenErrorMessage = "トークンが間違っています"
     }
   }
 }
